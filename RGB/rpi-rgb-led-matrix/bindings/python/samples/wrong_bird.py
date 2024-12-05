bbe7311d6ec5d330f75a935904d979f5e45c0fce
samuelsimonian <samuel_simonian1@baylor.edu>
2024-12-05 12:19:35 -0600

    bird bird bird

--- RGB/rpi-rgb-led-matrix/bindings/python/samples/wrong_bird.py
+++ RGB/rpi-rgb-led-matrix/bindings/python/samples/wrong_bird.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 from samplebase import SampleBase
 from smbus2 import SMBus, i2c_msg
+import os
 import time
-from rgbmatrix import graphics
-
-
 # I2C address of the sensor
 address = 0x18 
 
@@ -13,24 +11,25 @@ MINIMUM_PSI = 0
 OUTPUT_MAX = 0xE66666
 OUTPUT_MIN = 0x19999A
 bus = SMBus(1) 
-c = 0
+c = 32
+red = graphics.Color(255, 0, 0)
 
 def grass(self):
     for x in range (0, 32):
-        for y in range (28, 32):
+        for y in range (0, 4):
             self.matrix.SetPixel(x, y, 55, 148, 4)
 
 def bird(self, y_dist):
-    self.matrix.SetPixel(1, y_dist + 1, 213, 195, 0) # body
-    self.matrix.SetPixel(1, y_dist , 213, 195, 0) # body
-    self.matrix.SetPixel(2, y_dist + 1, 213, 195, 0) # body
-    self.matrix.SetPixel(2, y_dist , 213, 195, 0) # body
-    self.matrix.SetPixel(3, y_dist + 1, 255, 77, 68) # beak
-    self.matrix.SetPixel(4, y_dist + 1, 255, 77, 68) # beak
-    self.matrix.SetPixel(2, y_dist + 2, 246, 131, 11) # foot
-    self.matrix.SetPixel(3, y_dist , 227, 253, 218) # eye
-    bird_x = [1, 2, 3, 4]
-    bird_y = [y_dist, y_dist+1, y_dist+2, y_dist+3]
+    self.matrix.SetPixel(c - 2, c - y_dist + 1, 213, 195, 0) # body
+    self.matrix.SetPixel(c - 2, c - y_dist + 2, 213, 195, 0) # body
+    self.matrix.SetPixel(c - 3, c - y_dist + 1, 213, 195, 0) # body
+    self.matrix.SetPixel(c - 3, c - y_dist + 2, 213, 195, 0) # body
+    self.matrix.SetPixel(c - 4, c - y_dist + 1, 255, 77, 68) # beak
+    self.matrix.SetPixel(c - 5, c - y_dist + 1, 255, 77, 68) # beak
+    self.matrix.SetPixel(c - 3, c - y_dist, 246, 131, 11) # foot
+    self.matrix.SetPixel(c - 4, c - y_dist + 2, 227, 253, 218) # eye
+    bird_x = [c-2, c-3, c-4, c-5]
+    bird_y = [c-y_dist, c-y_dist+1, c-y_dist+2, c-y_dist+3]
     return bird_x, bird_y
 
 def pipe(self, x_pos, length):
@@ -38,25 +37,13 @@ def pipe(self, x_pos, length):
     pipe_y = []
     for x in range (x_pos, x_pos + 5):
         pipe_x.append(x)
-        for y in range (28 - length,28):
+        for y in range (4, 4 + length):
             # only append to pipe_y once
             if (x == x_pos):
                 pipe_y.append(y)
             self.matrix.SetPixel(x, y, 55, 195, 0)
         
     return pipe_x, pipe_y
-
-
-def top_pipe(self, x_pos, length):
-    top_pipe_x = []
-    top_pipe_y = []
-    for x in range (x_pos, x_pos + 5):
-        top_pipe_x.append(x)
-        for y in range (0, length):
-            if (x == x_pos):
-                top_pipe_y.append(y)
-            self.matrix.SetPixel(x, y, 55, 195, 0)
-    return top_pipe_x, top_pipe_y
 
 def check(self, b_x, b_y, p_x, p_y, t_p_x, t_p_y):
     for i in b_x:
@@ -77,34 +64,29 @@ def check(self, b_x, b_y, p_x, p_y, t_p_x, t_p_y):
                             return True
     return False
 
-def gg(self):
-    offscreen_canvas = self.matrix.CreateFrameCanvas()
-    font = graphics.Font()
-    font.LoadFont("../../../fonts/7x13.bdf")
-    textColor = graphics.Color(255, 0, 0)
-    pos = offscreen_canvas.width
-    my_text = self.args.text
-    x = 1
-    while (x > 0):
-        offscreen_canvas.Clear()
-        len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
-        pos -= 1
-        time.sleep(0.05)
-        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-        x = pos + len
+
+
+def top_pipe(self, x_pos, length):
+    top_pipe_x = []
+    top_pipe_y = []
+    for x in range (x_pos, x_pos + 5):
+        top_pipe_x.append(x)
+        for y in range (c - length, c):
+            if (x == x_pos):
+                top_pipe_y.append(y)
+            self.matrix.SetPixel(x, y, 55, 195, 0)
+    return top_pipe_x, top_pipe_y
 
 class GrayscaleBlock(SampleBase):
     def __init__(self, *args, **kwargs):
         super(GrayscaleBlock, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Game over!")
-
 
     def run(self):
         max_brightness = self.matrix.brightness # set to 100
         count = 0
         c = 255
         self.matrix.brightness = 75
-        x_pos = 35
+        x_pos = 0
         top_x_pos = 16
         while (True):
             time.sleep(0.1)
@@ -122,32 +104,29 @@ class GrayscaleBlock(SampleBase):
 
             y_dist = int(32 * (1-percent))
             #print(y_dist)
-            if y_dist < 0:
-                y_dist = 0
-            if y_dist > 25:
-                y_dist = 25
+            if y_dist < 3:
+                y_dist = 3
+            if y_dist > 28:
+                y_dist = 28
 
             
             self.matrix.Fill(63, 108 , 113)
             
-            x_pos -= 2
-            top_x_pos -= 1
+            x_pos += 2
+            top_x_pos += 1
 
-            if x_pos < -4:
-                x_pos = 35
-            if top_x_pos < -4:
-                top_x_pos = 35
+            if x_pos > 31:
+                x_pos = -4
+            if top_x_pos > 31:
+                top_x_pos = -4
 
             pipe_x, pipe_y = pipe(self, x_pos, 9)
             top_pipe_x, top_pipe_y = top_pipe(self, top_x_pos, 9)
             bird_x, bird_y = bird(self, y_dist)
             grass(self)
             if check(self, bird_x, bird_y, pipe_x, pipe_y, top_pipe_x, top_pipe_y):
-                x_pos = 35
-                top_x_pos = 35
-                self.matrix.Fill(0,0,0)
-                time.sleep(0.05)
-                gg(self)
+                os.system("python /home/pi/Documents/Art_Embedded_Project/RGB/rpi-rgb-led-matrix/bindings/python/samples/runtext.py ")
+                time.sleep(5)
 
 
 # Main function
